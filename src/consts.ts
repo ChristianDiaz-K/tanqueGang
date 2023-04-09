3b5a0fb38d1d6d5fef6f174ef147cf99af963793
Christian <ch.diazavila@gmail.com>
2023-04-09 05:56:37 -0400

    Videos

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Web oficial de la Tanque Gang.';
+export const SITE_TITLE = 'Tanque Gang.';
 export const SITE_DESCRIPTION = 'Tanque Gang Pagina oficial';
