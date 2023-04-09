56bf7c529d5e6d19bd62a434d5c61dfca0e79064
Christian <ch.diazavila@gmail.com>
2023-04-09 03:24:55 -0400

    Iniciando

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Web oficial de la Tanque Gang.';
+export const SITE_DESCRIPTION = 'Tanque Gang Pagina oficial';
